720b0d52226924530d35f257b56cceac7c1d0d0c
Missael <lmpadillar@gmail.com>
2026-06-10 18:58:16 -0700

    fix tests

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,12 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+const baseUrl = process.env.BASE_URL || 'https://gentle-marigold-f653a5.netlify.app/';
 
 test('happy path: register y login', async ({ page }) => {
-    const email = `qa+${Date.now()}@test.com`;
+    const email = `qaTest@test.com`;
     const password = 'QaTest123!';
 
     await page.goto(`${baseUrl}/register`);
+
+    await page.waitForLoadState('networkidle');
+
+    await page.waitForFunction(() => {
+    return document.querySelector('#__nuxt') !== null;
+    });
 
     await page.fill('#email', email);
     await page.fill('#password', password);
@@ -21,11 +27,27 @@ test('happy path: register y login', async ({ page }) => {
         fullPage: true,
     });
 
+    console.log(
+  'Antes click:',
+  await page.locator('#email').inputValue()
+);
+
+await page.waitForTimeout(1000);
+
+console.log(
+  '1 segundo después:',
+  await page.locator('#email').inputValue()
+);
+
     await page.click('button[type="submit"]');
 
     await page.waitForTimeout(2000);
 
     console.log('URL after submit:', await page.url());
+
+
+    await page.waitForTimeout(8000);
+
 
     await expect(page).toHaveURL(/login/);
 
