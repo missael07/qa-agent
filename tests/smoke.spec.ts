23209810482463e14f3f217e48f59fe5c8428fdd
Missael <lmpadillar@gmail.com>
2026-06-10 20:07:16 -0700

    fix: add wait timeout after form submission in smoke test

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -3,52 +3,55 @@ import { test, expect } from '@playwright/test';
 const baseUrl = process.env.BASE_URL || 'https://gentle-marigold-f653a5.netlify.app/';
 
 test('happy path: register y login', async ({ page }) => {
-    const email = `qaTest@test.com`;
-    const password = 'QaTest123!';
+  const email = `qaTest@test.com`;
+  const password = 'QaTest123!';
 
-    await page.goto(`${baseUrl}/register`);
+  await page.goto(`${baseUrl}/register`);
 
-    await page.waitForLoadState('networkidle');
+  await page.waitForLoadState('networkidle');
 
-    await page.waitForFunction(() => {
+  await page.waitForFunction(() => {
     return document.querySelector('#__nuxt') !== null;
-    });
+  });
 
-    await page.fill('#email', email);
-    await page.fill('#password', password);
-    await page.fill('#confirmPassword', password);
+  await page.fill('#email', email);
+  await page.fill('#password', password);
+  await page.fill('#confirmPassword', password);
 
-    console.log('Email:', await page.locator('#email').inputValue());
-    console.log('Password:', await page.locator('#password').inputValue());
-    console.log('Confirm:', await page.locator('#confirmPassword').inputValue());
+  console.log('Email:', await page.locator('#email').inputValue());
+  console.log('Password:', await page.locator('#password').inputValue());
+  console.log('Confirm:', await page.locator('#confirmPassword').inputValue());
 
-    await page.screenshot({
-        path: 'before-submit.png',
-        fullPage: true,
-    });
+  await page.screenshot({
+    path: 'before-submit.png',
+    fullPage: true,
+  });
 
-    console.log(
-  'Antes click:',
-  await page.locator('#email').inputValue()
-);
+  console.log(
+    'Antes click:',
+    await page.locator('#email').inputValue()
+  );
 
-await page.waitForTimeout(1000);
+  await page.waitForTimeout(1000);
 
-console.log(
-  '1 segundo después:',
-  await page.locator('#email').inputValue()
-);
+  console.log(
+    '1 segundo después:',
+    await page.locator('#email').inputValue()
+  );
 
-    await page.click('button[type="submit"]');
+  await page.click('button[type="submit"]');
+
+  await page.waitForTimeout(3000);
 
 
-    console.log('URL after submit:', await page.url());
 
-    await expect(page).toHaveURL(/login/);
+  console.log('URL after submit:', await page.url());
 
-    await page.fill('input[name="email"]', email);
-    await page.fill('input[name="password"]', password);
-    await page.click('button[type="submit"]');
+  await expect(page).toHaveURL(/login/);
 
-    await expect(page).toHaveURL(/dashboard/);
+  await page.fill('input[name="email"]', email);
+  await page.fill('input[name="password"]', password);
+  await page.click('button[type="submit"]');
+
+  await expect(page).toHaveURL(/dashboard/);
 });
